64efd626c60d2cdbad2d720f7ff857f046bedfc5
USER <user@USER-no-MacBook-Air-2.local>
2015-03-07 15:32:15 +0900

    Initial Commit

---
+++ egg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  egg
+//
+//  Created by USER on 2015/03/07.
+//  Copyright (c) 2015年 USER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ egg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  egg
+//
+//  Created by USER on 2015/03/07.
+//  Copyright (c) 2015年 USER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
